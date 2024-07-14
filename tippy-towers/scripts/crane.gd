8ebc855588d1cce3b3b387cd957c940cf9f95db0
Max Aller <nanodeath@gmail.com>
2024-07-14 12:14:46 -0700

    Fix the anchor and rails on the crane a bit.

--- tippy-towers/scripts/crane.gd
+++ tippy-towers/scripts/crane.gd
@@ -1,6 +1,8 @@
 class_name Crane extends CharacterBody2D
 
 @export var speed: int
+@export var move_up_speed: int
+@onready var chain_anchor = $ChainAnchor
 
 func _ready():
 	pass # Replace with function body.
@@ -10,3 +12,8 @@ func _physics_process(delta):
 		position.x -= speed * delta
 	elif Input.is_action_pressed("move_right"):
 		position.x += speed * delta
+	
+	if Input.is_action_pressed("move_up"):
+		chain_anchor.position.y -= move_up_speed * delta
+	elif Input.is_action_pressed("move_down"):
+		chain_anchor.position.y += move_up_speed * delta
