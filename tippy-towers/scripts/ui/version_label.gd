031801c7404b04ab931cf65c6fe501bf3da272c8
Max Aller <nanodeath@gmail.com>
2024-07-15 06:19:09 -0700

    Add version label so I can see what platform detection capabilities we have.

---
+++ tippy-towers/scripts/ui/version_label.gd
@@ -0,0 +1,12 @@
+extends RichTextLabel
+
+func _ready():
+	text = str("web:", OS.has_feature("web"),",web_android:", OS.has_feature("web_android"),",is_mobile_web:", is_mobile_web())
+
+func is_mobile_web():
+	if OS.has_feature("JavaScript"):
+		# Use JavaScript to detect mobile browsers
+		return JavaScriptBridge.eval("""
+			/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+		""")
+	return false
