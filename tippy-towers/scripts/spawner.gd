eb7a845823057b6f51aebd6eea8bb06f255eca29
Max Aller <nanodeath@gmail.com>
2024-07-15 19:06:07 -0700

    Create level 2 spawner group and isosceles triangles.

--- tippy-towers/scripts/spawner.gd
+++ tippy-towers/scripts/spawner.gd
@@ -2,13 +2,25 @@ class_name Spawner extends Node2D
 
 @export var to_spawn: Array[PackedScene]
 @onready var spawn_timer = %SpawnTimer
+@onready var packages_scored := %PackagesScored as PackagesScored
+
+var current_spawner_group: SpawnerGroup
 
 func _ready():
 	spawn.call_deferred()
 	spawn_timer.tick.connect(self.spawn)
+	_update_current_group()
 
 func spawn():
-	var instance := to_spawn.pick_random().instantiate() as Node2D
+	_update_current_group()
+	var instance := current_spawner_group.to_spawn.pick_random().instantiate() as Node2D
 	instance.position = position
 	instance.rotation = randf_range(0, PI * 2)
 	add_sibling(instance)
+
+func _update_current_group():
+	var packages_stacked := packages_scored.packages_scored
+	for c in get_children():
+		var spawner_group := c as SpawnerGroup
+		if packages_stacked >= spawner_group.package_threshold:
+			current_spawner_group = spawner_group
---
+++ tippy-towers/scripts/spawner_group.gd
@@ -0,0 +1,4 @@
+class_name SpawnerGroup extends Node
+
+@export var to_spawn: Array[PackedScene]
+@export var package_threshold: int
