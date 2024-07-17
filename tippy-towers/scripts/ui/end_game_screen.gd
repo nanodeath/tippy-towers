70bdc51f0875892c2b8da5371c2ac9072890705b
Max Aller <nanodeath@gmail.com>
2024-07-16 19:37:46 -0700

    Wire current day into endgame screen.

--- tippy-towers/scripts/ui/end_game_screen.gd
+++ tippy-towers/scripts/ui/end_game_screen.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var height_achieved_count = $PanelContainer/CenterContainer/VBoxContainer/GridContainer/HeightAchievedCount
 @onready var days_since_last_accident_counter = $PanelContainer/CenterContainer/VBoxContainer/DaysSinceLastAccidentCounter
 @onready var package_lost_zone = %PackageLostZone
+@onready var day_manager: DayManager = %DayManager
 
 func _ready():
 	get_node("/root/Game").game_over.connect(activate)
@@ -12,5 +13,5 @@ func activate():
 	visible = true
 	packages_placed_count.text = str(%PackagesScored.packages_scored)
 	height_achieved_count.text = str(%HighestPointDetector.maximum_height)
-	var days := floori(%HighestPointDetector.maximum_height / 100)
+	var days := day_manager.current_day + 1
 	days_since_last_accident_counter.text = str(days)
