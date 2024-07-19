7e8ab2bfd78339d843e682841a0b08ff8499b81f
Max Aller <nanodeath@gmail.com>
2024-07-19 07:38:13 -0700

    Adding noise to all boxes except the triangle box.

--- tippy-towers/scripts/box.gd
+++ tippy-towers/scripts/box.gd
@@ -1,8 +1,12 @@
 class_name Box extends RigidBody2D
 
 @export var sprites: Array[Sprite2D]
+@onready var noise: Sprite2D = $Noise
 
 func _ready():
 	var sprite_to_show := sprites.pick_random() as Sprite2D
 	for s in sprites:
 		s.visible = s == sprite_to_show
+	var noise_texture := noise.texture.duplicate(true) as NoiseTexture2D
+	(noise_texture.noise as FastNoiseLite).seed = randi()
+	noise.texture = noise_texture
